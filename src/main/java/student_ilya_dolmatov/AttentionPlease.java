3390058ac14225279b240a020249a162361dca61
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-06-07 20:25:06 +0300

    Add classes

---
+++ src/main/java/student_ilya_dolmatov/AttentionPlease.java
@@ -0,0 +1,8 @@
+package student_ilya_dolmatov;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
---
+++ src/main/java/student_ilya_dolmatov/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package student_ilya_dolmatov;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
