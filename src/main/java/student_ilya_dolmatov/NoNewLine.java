400fe2fa6d4461dafbc1fd21058267051dd55aef
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-06-07 20:16:49 +0300

    Add classes

---
+++ src/main/java/student_ilya_dolmatov/NoNewLine.java
@@ -0,0 +1,8 @@
+package student_ilya_dolmatov;
+
+public class NoNewLine {
+    public static void main(String[] args) {
+        System.out.print("1st line");
+        System.out.print("2nd line");
+    }
+}
