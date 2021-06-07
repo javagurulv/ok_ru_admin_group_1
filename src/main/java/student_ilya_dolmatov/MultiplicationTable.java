92323ac45acb7a54997a34b94739739926ae1a54
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-06-07 19:48:58 +0300

    Add MultiplicationTable class

---
+++ src/main/java/student_ilya_dolmatov/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package student_ilya_dolmatov;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Введите число для вывода таблицы умножения:");
+        Scanner scanner = new Scanner(System.in);
+        int digit = scanner.nextInt();
+
+        System.out.println("Test Data:\nInput a number: " + digit + "\n\nExpected Output :");
+        int res;
+        for(int i = 1; i<=10; i++){
+            res = digit * i;
+            System.out.println("\t" + digit + " x " + i + " = " + res);
+        }
+    }
+}
