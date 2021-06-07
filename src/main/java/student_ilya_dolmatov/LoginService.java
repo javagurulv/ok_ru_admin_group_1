3926b3cd8838bd80aef24f66d1ab2b830f08d5dc
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-06-07 19:39:44 +0300

    Fixed for compile

---
+++ src/main/java/student_ilya_dolmatov/LoginService.java
@@ -0,0 +1,7 @@
+package student_ilya_dolmatov;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
---
+++ src/main/java/student_ilya_dolmatov/Turtle.java
@@ -0,0 +1,5 @@
+package student_ilya_dolmatov;
+
+public class Turtle {
+    public static void main(String[] args) {}
+}
