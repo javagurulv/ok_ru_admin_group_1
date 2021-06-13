404f6e898b92d3181888c96fd1fa13516b2a7c2e
vsavonins <viktors.savonins@proofit.lv>
2021-06-13 14:06:32 +0300

    code review!

--- src/main/java/student_ilya_dolmatov/lesson_1/level_4/task_11/PrintSum.java
+++ src/main/java/student_ilya_dolmatov/lesson_1/level_4/task_11/PrintSum.java
@@ -1,5 +1,8 @@
 package student_ilya_dolmatov.lesson_1.level_4.task_11;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class PrintSum {
     public static void main(String[] args) {
         System.out.println("Тестовые данные:\n74 + 36\nОжидаемый результат:");
