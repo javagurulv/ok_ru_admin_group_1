38def8b0581a658673431d5724227cd079ff2767
vsavonins <viktors.savonins@proofit.lv>
2021-06-13 14:11:44 +0300

    code review!

--- src/main/java/student_sergey_egorichev/lesson_1/task_10/HelloWorld.java
+++ src/main/java/student_sergey_egorichev/lesson_1/task_10/HelloWorld.java
@@ -1,5 +1,8 @@
 package student_sergey_egorichev.lesson_1.task_10;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("I released my finger from the trigger. And then it was over. © Max Payne");
--- src/main/java/student_sergey_egorichev/lesson_1/task_11/PrintSum.java
+++ src/main/java/student_sergey_egorichev/lesson_1/task_11/PrintSum.java
@@ -1,5 +1,8 @@
 package student_sergey_egorichev.lesson_1.task_11;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class PrintSum {
     public static void main(String [] args) {
         System.out.println(409+42);
