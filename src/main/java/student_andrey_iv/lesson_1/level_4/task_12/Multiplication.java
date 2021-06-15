49aa76f9b1497f87fd602dd08b55cb48a430cca3
andrey.ivan <andrey@ivvan.com>
2021-06-15 13:49:56 +0300

    add student Andrey_IV_lesson_1.level_4.task_12

---
+++ src/main/java/student_andrey_iv/lesson_1/level_4/task_12/Multiplication.java
@@ -0,0 +1,8 @@
+package student_andrey_iv.lesson_1.level_4.task_12;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        System.out.println("The result of the multiplication \n10*10\nis:");
+        System.out.println(10*10);
+    }
+}
