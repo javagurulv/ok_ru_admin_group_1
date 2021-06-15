22f5df116115e3456fec7f7e31d0016fa5d97af8
Ivan Buymov <ivan@buymov.ru>
2021-06-15 21:28:25 +0300

    homework lesson2, level 1, task 1

---
+++ src/main/java/student_ivan_buymov/lesson_2/level_1_task_1.java
@@ -0,0 +1,40 @@
+package student_ivan_buymov.lesson_2;
+
+import java.util.Scanner;
+
+public class level_1_task_1 {
+    public static void main(String[] args) {
+        doMathOperations();
+    }
+
+    private static Integer readInt(String message, Scanner s) {
+        do {
+            System.out.print(message);
+            if (s.hasNextInt()) {
+                return s.nextInt();
+            } else {
+                System.out.println("number must be a positive integer, try again");
+                s.nextLine();
+            }
+        } while (true);
+    }
+
+    private static void doMathOperations() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = readInt("Input first number: ", scanner);
+        int secondNumber = readInt("Input second number: ", scanner);
+
+        String sumResult = String.format("%d + %d = %d", firstNumber, secondNumber, firstNumber + secondNumber);
+        String subtractResult = String.format("%d - %d = %d", firstNumber, secondNumber, firstNumber - secondNumber);
+        String multiplyResult = String.format("%d * %d = %d", firstNumber, secondNumber, firstNumber * secondNumber);
+        String divisionResult = String.format("%d / %d = %d", firstNumber, secondNumber, firstNumber / secondNumber);
+
+        System.out.println(sumResult);
+        System.out.println(subtractResult);
+        System.out.println(multiplyResult);
+        System.out.println(divisionResult);
+
+    }
+}
