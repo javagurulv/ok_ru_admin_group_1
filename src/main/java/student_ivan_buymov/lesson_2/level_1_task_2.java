739bbf413269c5c7b430e39c00271cfd2e405bf4
Ivan Buymov <ivan@buymov.ru>
2021-06-15 21:47:25 +0300

    homework lesson2, level 1, task 2

---
+++ src/main/java/student_ivan_buymov/lesson_2/level_1_task_2.java
@@ -0,0 +1,40 @@
+package student_ivan_buymov.lesson_2;
+
+import java.util.Scanner;
+
+public class level_1_task_2 {
+    public static void main(String[] args) {
+        doMathOperations();
+    }
+
+    private static Double readDouble(String message, Scanner s) {
+        do {
+            System.out.print(message);
+            if (s.hasNextDouble()) {
+                return s.nextDouble();
+            } else {
+                System.out.println("number must be a double, try again");
+                s.nextLine();
+            }
+        } while (true);
+    }
+
+    private static void doMathOperations() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double firstNumber = readDouble("Input first number: ", scanner);
+        double secondNumber = readDouble("Input second number: ", scanner);
+
+        String sumResult = String.format("%f + %f = %f", firstNumber, secondNumber, firstNumber + secondNumber);
+        String subtractResult = String.format("%f - %f = %f", firstNumber, secondNumber, firstNumber - secondNumber);
+        String multiplyResult = String.format("%f * %f = %f", firstNumber, secondNumber, firstNumber * secondNumber);
+        String divisionResult = String.format("%f / %f = %f", firstNumber, secondNumber, firstNumber / secondNumber);
+
+        System.out.println(sumResult);
+        System.out.println(subtractResult);
+        System.out.println(multiplyResult);
+        System.out.println(divisionResult);
+
+    }
+}
