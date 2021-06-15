4b38254f50db8e80b7c38802ee2f3d9312b0d1ab
Ivan Buymov <ivan@buymov.ru>
2021-06-15 11:25:19 +0300

    homework for lesson 1

---
+++ src/main/java/student_ivan_buymov/lesson_1/lesson_1.java
@@ -0,0 +1,23 @@
+package student_ivan_buymov.lesson_1;
+
+public class lesson_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(74, 36));
+        System.out.println(multiply(50, 3));
+
+    }
+
+    public static Integer sum(int a, int b)  {
+
+        return a + b;
+
+    }
+
+    public static Integer multiply(int a, int b) {
+        return a * b;
+    }
+
+
+}
