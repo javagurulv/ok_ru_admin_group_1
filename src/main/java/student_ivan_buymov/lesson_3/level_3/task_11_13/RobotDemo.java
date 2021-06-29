e0565fd377911348a91413b7cfa9eee978f66255
Ivan Buymov <ivan@buymov.ru>
2021-06-29 23:15:33 +0300

    add lesson 4 homework: part 1

---
+++ src/main/java/student_ivan_buymov/lesson_3/level_3/task_11_13/RobotDemo.java
@@ -0,0 +1,15 @@
+package student_ivan_buymov.lesson_3.level_3.task_11_13;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        var robotOne = new Robot("One");
+        robotOne.sayHello();
+        robotOne.sayYourName();
+
+        var robotTwo = new Robot("Two");
+        robotTwo.sayYourName();
+
+    }
+
+}
---
+++ src/main/java/student_ivan_buymov/lesson_3/level_5/task_18_22/DogDemo.java
@@ -0,0 +1,11 @@
+package student_ivan_buymov.lesson_3.level_5.task_18_22;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+        var dog = new Dog("Wilfred");
+
+        dog.voice();
+    }
+
+}
