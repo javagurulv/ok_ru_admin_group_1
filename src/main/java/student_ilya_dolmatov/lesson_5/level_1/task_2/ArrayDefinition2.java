5e42b80d8c63011b2ac3620d4328d732a0409f04
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-07-12 22:56:42 +0300

    Add level 1 for lesson 5

---
+++ src/main/java/student_ilya_dolmatov/lesson_5/level_1/task_2/ArrayDefinition2.java
@@ -0,0 +1,7 @@
+package student_ilya_dolmatov.lesson_5.level_1.task_2;
+
+class ArrayDefinition2 {
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
+}
---
+++ src/main/java/student_ilya_dolmatov/lesson_5/level_1/task_5/ArrayDefinition5.java
@@ -0,0 +1,7 @@
+package student_ilya_dolmatov.lesson_5.level_1.task_5;
+
+class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1, 2, 3, 4, };
+    }
+}
