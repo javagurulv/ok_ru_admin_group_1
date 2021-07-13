804d17c4339aa56eb6b6342769ce171f62a4c412
Ilya Dolmatov <ilya.dolmatov@corp.mail.ru>
2021-07-13 12:27:17 +0300

    Add level 3 for lesson 5

---
+++ src/main/java/student_ilya_dolmatov/lesson_5/level_3/task_24/ForLoopDefinition8.java
@@ -0,0 +1,9 @@
+package student_ilya_dolmatov.lesson_5.level_3.task_24;
+
+class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
