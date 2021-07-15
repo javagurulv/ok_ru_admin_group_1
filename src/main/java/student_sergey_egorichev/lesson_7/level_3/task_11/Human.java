f77763b3d8aae1ecfd789b76bd071ccf75b05fb2
Sergey Egorichev <sergey.egorichev@corp.mail.ru>
2021-07-15 10:46:29 +0300

    Lesson_7: Homework Level_3, task_11

---
+++ src/main/java/student_sergey_egorichev/lesson_7/level_3/task_11/Human.java
@@ -0,0 +1,4 @@
+package student_sergey_egorichev.lesson_7.level_3.task_11;
+
+public class Human {
+}
---
+++ src/main/java/student_sergey_egorichev/lesson_7/level_3/task_11/LoanSystem.java
@@ -0,0 +1,7 @@
+package student_sergey_egorichev.lesson_7.level_3.task_11;
+
+abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+
+}
