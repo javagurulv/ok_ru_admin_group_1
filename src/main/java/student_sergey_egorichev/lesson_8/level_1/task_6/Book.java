2051f4768234d613b4a2394ec7875dbe9fdf1786
Sergey Egorichev <sergey.egorichev@corp.mail.ru>
2021-07-19 19:08:27 +0300

    Lesson_8: Homework Level_1, task_6,7

---
+++ src/main/java/student_sergey_egorichev/lesson_8/level_1/task_6/Book.java
@@ -0,0 +1,23 @@
+package student_sergey_egorichev.lesson_8.level_1.task_6;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/student_sergey_egorichev/lesson_8/level_1/task_7/PrivateClass.java
@@ -0,0 +1,9 @@
+package student_sergey_egorichev.lesson_8.level_1.task_7;
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+
+
+}
