b6ec9a30c301c04397c19600f24317a1235064a5
Sergey Egorichev <sergey.egorichev@corp.mail.ru>
2021-07-19 18:21:09 +0300

    Lesson_8: Homework Level_1

---
+++ src/main/java/student_sergey_egorichev/lesson_8/level_1/task_2/Rocket.java
@@ -0,0 +1,4 @@
+package student_sergey_egorichev.lesson_8.level_1.task_2;
+
+class Rocket {
+}
---
+++ src/main/java/student_sergey_egorichev/lesson_8/level_1/task_3/PrivateClass.java
@@ -0,0 +1,11 @@
+package student_sergey_egorichev.lesson_8.level_1.task_3;
+
+/* Modifier 'private' not allowed here (сообщение от IDE)
+Класс не может быть приватным, т.к это лишено смысла.
+Если класс нельзя использовать, то зачем нам этот класс вообще нужен.
+Приватными могут быть его аттрибуты и методы которые мы хотим скрыть.
+ */
+class PrivateClass {
+    private int parameterOne;
+    private int parameterTwo;
+}
