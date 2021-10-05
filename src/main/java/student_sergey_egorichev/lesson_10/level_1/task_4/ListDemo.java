9bb594dbd3bc843f1e8af562d315d37284620911
Sergey Egorichev <sergey.egorichev@corp.mail.ru>
2021-10-05 20:37:42 +0300

    Lesson_10: Homework Level_1 Task_4

---
+++ src/main/java/student_sergey_egorichev/lesson_10/level_1/task_4/ListDemo.java
@@ -0,0 +1,21 @@
+package student_sergey_egorichev.lesson_10.level_1.task_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListDemo {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add("one");
+        list.add("two");
+        list.add("one");
+        list.add("three");
+
+        System.out.println(list);
+
+    }
+
+}
