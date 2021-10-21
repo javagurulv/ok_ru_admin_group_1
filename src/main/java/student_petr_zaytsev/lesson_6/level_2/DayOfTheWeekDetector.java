cac0688c1eb7afb102f1f57d06330319f7ac0854
Petr Zaytsev <petr.zaytsev@corp.mail.ru>
2021-10-21 18:49:01 +0300

    homework

---
+++ src/main/java/student_petr_zaytsev/lesson_6/level_2/DayOfTheWeekDetector.java
@@ -0,0 +1,40 @@
+package student_petr_zaytsev.lesson_6.level_2;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetector {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String result = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(result);
+    }
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public String findDayOfTheWeek(int dayNumber) {
+        switch (dayNumber) {
+            case 1 :
+                return "Monday";
+            case 2 :
+                return "Tuesday";
+            case 3 :
+                return "Wednesday";
+            case 4 :
+                return "Thursday";
+            case 5 :
+                return "Friday";
+            case 6 :
+                return "Saturday";
+            case 7 :
+                return "Sunday";
+            default :
+                return "Not correct day number";
+        }
+    }
+}
