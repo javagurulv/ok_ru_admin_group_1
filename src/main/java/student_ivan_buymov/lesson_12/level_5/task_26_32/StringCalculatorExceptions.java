fcbdb74496b791bae9935282f51d94effbb0e828
Ivan Buymov <ivan@buymov.ru>
2021-10-24 17:03:38 +0300

    add lesson 12

---
+++ src/main/java/student_ivan_buymov/lesson_12/level_5/task_26_32/StringCalculatorExceptions.java
@@ -0,0 +1,11 @@
+package student_ivan_buymov.lesson_12.level_5.task_26_32;
+
+public class StringCalculatorExceptions {
+
+    public static class IncorrectInputFormat extends Exception {
+        public IncorrectInputFormat(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+}
